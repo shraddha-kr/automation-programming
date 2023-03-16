d51a280fe7eef5bda2872589a85ed2fa0d18aa7c
Shraddha-kr <shraddhakr@outlook.com>
2023-03-16 17:18:00 +0530

    16 Mar

---
+++ selenium-java/automation-java/src/alert_ex.java
@@ -0,0 +1,17 @@
+// //https://www.softwaretestingmaterial.com/javascript-alerts-popups-selenium/
+// System.setProperty("webdriver.chrome.driver", "D:\\Selenium Environment\\Drivers\\chromedriver.exe");
+// 		WebDriver driver = new ChromeDriver();
+// 		driver.get("http://softwaretestingplace.blogspot.com/2017/03/javascript-alert-test-page.html");
+// 		driver.findElement(By.xpath("//*[@id='content']/button")).click();
+// 		Thread.sleep(3000);
+// 		Alert alert = driver.switchTo().alert();
+// 		String print = alert.getText();
+// 		System.out.println(print);
+// 		alert.accept();
+// 		Thread.sleep(3000);
+// 		driver.findElement(By.xpath("//*[@id='content']/button")).click();
+// 		Thread.sleep(3000);
+// 		alert.dismiss();
+// 		driver.close();
+// 	}
+// }
